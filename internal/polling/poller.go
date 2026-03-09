96ca8351550a2a294677f216bc84ac9ed4748e2c
Kim Valkama <hello@valkama.kim>
2026-03-09 22:40:40 +0200

    Connection fix

--- internal/polling/poller.go
+++ internal/polling/poller.go
@@ -1,11 +1,12 @@
 package polling
 
 import (
-	"strings"
 	"encoding/json"
-	"sealboard/internal/config"
-	"sealboard/internal/util"
 	"sealboard/internal/api"
+	"sealboard/internal/config"
+	"sealboard/internal/tunnel"
+	"sealboard/internal/util"
+	"strings"
 )
 
 // pollHosts polls all hosts for Podman stats via Unix socket
@@ -17,52 +18,55 @@ func PollHosts(hosts []config.HostConfig, podmanStatsCache *api.PodmanStatsCache
 				sp := host.SocketPath
 				sp = util.ExpandUIDVariable(sp)
 				for _, endpoint := range api.PodmanLibpodEndpoints {
-											if strings.Contains(endpoint, "{id}") {
-												// Fetch the container list first
-												cjsonEndpoint := "http://d/v4.0.0/libpod/containers/json?all=true"
-												api.CallPodmanAPIUnixEndpoint(sp, host.Name, cjsonEndpoint, podmanStatsCache)
-												cjson, ok := podmanStatsCache.Get(host.Name, cjsonEndpoint)
-												if ok {
-													var containers []map[string]interface{}
-													_ = json.Unmarshal(cjson, &containers)
-													for _, c := range containers {
-														if id, ok := c["Id"].(string); ok {
-															fullEndpoint := api.InterpolateEndpoint(endpoint, map[string]string{"id": id})
-															api.CallPodmanAPIUnixEndpoint(sp, host.Name, fullEndpoint, podmanStatsCache)
-														}
-													}
-												}
-											} else {
-												api.CallPodmanAPIUnixEndpoint(sp, host.Name, endpoint, podmanStatsCache)
-											}
+					if strings.Contains(endpoint, "{id}") {
+						// Fetch the container list first
+						cjsonEndpoint := "http://d/v4.0.0/libpod/containers/json?all=true"
+						api.CallPodmanAPIUnixEndpoint(sp, host.Name, cjsonEndpoint, podmanStatsCache)
+						cjson, ok := podmanStatsCache.Get(host.Name, cjsonEndpoint)
+						if ok {
+							var containers []map[string]any
+							_ = json.Unmarshal(cjson, &containers)
+							for _, c := range containers {
+								if id, ok := c["Id"].(string); ok {
+									fullEndpoint := api.InterpolateEndpoint(endpoint, map[string]string{"id": id})
+									api.CallPodmanAPIUnixEndpoint(sp, host.Name, fullEndpoint, podmanStatsCache)
+								}
 							}
-				
+						}
+					} else {
+						api.CallPodmanAPIUnixEndpoint(sp, host.Name, endpoint, podmanStatsCache)
+					}
+				}
+
 			}
 			continue
 		}
 		if host.LocalSocketPath != "" && host.RemoteSocketPath != "" {
+			// Ensure tunnel is alive before polling
+			// (new: calls tunnel.EnsureTunnel)
+			tunnel.EnsureTunnel(host)
+
 			lsp := host.LocalSocketPath
 			for _, endpoint := range api.PodmanLibpodEndpoints {
-						if strings.Contains(endpoint, "{id}") {
-							// Fetch container list from remote socket
-							cjsonEndpoint := "http://d/v4.0.0/libpod/containers/json?all=true"
-							api.CallPodmanAPIUnixEndpoint(lsp, host.Name, cjsonEndpoint, podmanStatsCache)
-							cjson, ok := podmanStatsCache.Get(host.Name, cjsonEndpoint)
-							if ok {
-								var containers []map[string]interface{}
-								_ = json.Unmarshal(cjson, &containers)
-								for _, c := range containers {
-									if id, ok := c["Id"].(string); ok {
-										fullEndpoint := api.InterpolateEndpoint(endpoint, map[string]string{"id": id})
-										api.CallPodmanAPIUnixEndpoint(lsp, host.Name, fullEndpoint, podmanStatsCache)
-									}
-								}
+				if strings.Contains(endpoint, "{id}") {
+					// Fetch container list from remote socket
+					cjsonEndpoint := "http://d/v4.0.0/libpod/containers/json?all=true"
+					api.CallPodmanAPIUnixEndpoint(lsp, host.Name, cjsonEndpoint, podmanStatsCache)
+					cjson, ok := podmanStatsCache.Get(host.Name, cjsonEndpoint)
+					if ok {
+						var containers []map[string]any
+						_ = json.Unmarshal(cjson, &containers)
+						for _, c := range containers {
+							if id, ok := c["Id"].(string); ok {
+								fullEndpoint := api.InterpolateEndpoint(endpoint, map[string]string{"id": id})
+								api.CallPodmanAPIUnixEndpoint(lsp, host.Name, fullEndpoint, podmanStatsCache)
 							}
-						} else {
-							api.CallPodmanAPIUnixEndpoint(lsp, host.Name, endpoint, podmanStatsCache)
 						}
 					}
+				} else {
+					api.CallPodmanAPIUnixEndpoint(lsp, host.Name, endpoint, podmanStatsCache)
+				}
+			}
 		}
 	}
 }
-
